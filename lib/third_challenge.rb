58cd9850a1ecdf8da32173c4c61e64ee687f1ce0
Altaf M Quadri <altafmquadri@gmail.com>
2019-08-30 06:18:21 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -18,10 +18,11 @@ def third_challenge
    }, 
    :capulet => {
       :patriarch => {
-        :name => "Lord Capulet"
-        :age => "50"},
+        :name => "Lord Capulet",
+        :age => "50"
+        },
       :matriarch => {
-        :name => "Lady Capulet"
+        :name => "Lady Capulet",
         :age => "51"
         },
       :heroine => {
